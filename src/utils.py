975b3b592c50c8f49346c0d3a7b4f8476efb7fdd
Nikolay Gendin <gendin786@gmail.com>
2025-01-22 00:20:25 +0300

    added tests utils.py with modify

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,16 @@
 import json
-import os
 
 
 def load_transactions(file_path: str) -> list:
     """Принимает путь до файла JSON и возвращает из него список словарей операций"""
     try:
-        if os.path.exists(file_path):
-            with open(file_path, "r", encoding="UTF-8") as json_file:
-                operations = json.load(json_file)
-                if isinstance(operations, list):
-                    return operations
-                else:
-                    return []
-        else:
-            return []
+        with open(file_path, "r", encoding="UTF-8") as json_file:
+            data = json.load(json_file)
+            return data if isinstance(data, list) else []
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
     except Exception as e:
-        print(f"Something went wrong: {e}")
+        print(f"Unexpected error: {e}")
         return []
